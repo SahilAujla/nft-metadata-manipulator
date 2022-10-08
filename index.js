5b8cde35aba9ea3391064578966203822a8d582e
Sahil Aujla <sahilaujla15@gmail.com>
2022-10-07 21:36:09 -0400

    refactor code

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
 
-for (let i = 0; i < 37364; i++) {
-    let rawdata = fs.readFileSync(`json/${i + 1}.json`);
+let numberOfNFTs = 100;
+let nftStartTokenId = 1;
+
+for (let i = 0; i < numberOfNFTs; i++) {
+    let rawdata = fs.readFileSync(`json/${i + nftStartTokenId}.json`);
+
     let parsedData = JSON.parse(rawdata);
-    delete parsedData['edition'];
-    delete parsedData['dna'];
-    // delete parsedData['compiler'];
-    // parsedData['createdBy'] = "Sahil";
+
+    delete parsedData['edition']; // will delete the edition attribute from all the json file under the json folder
+
+    delete parsedData['dna']; // will  delete the DNA attribute from all the json file under the json folder
+
+    delete parsedData['compiler']; // will  delete the compiler attribute from all the json file under the json folder
+
+    parsedData['createdBy'] = "Sahil"; // will add a new attribute with the key of createdBy and value of Sahil to all the  metadata files.
+
     let data = JSON.stringify(parsedData, null, 2);
+
     fs.writeFileSync(`json/${i + 1}.json`, data);
+    
     console.log(parsedData);
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# NFT Metadata Manipulator
+Often times when we use tools like Hashlips art engine to generate NFT metadata there are certain attributes that are added to all the metadata files that we don't need in our collection. For example the `compiler` attribute or the `date` attribute.
+
+This script can help you add/remove any attributes to thousands of your metadata files at once!
+
+Just place your metadata files under the json folder, set your configuration in the `index.js` file and run the `index.js` script to update your metadata.
